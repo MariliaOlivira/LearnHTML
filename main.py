9b576e122b6dfa421e006932716dedc3a77c105d
Marilia <marilia.araujo@academico.ifpb.edu.br>
2022-06-30 19:37:46 -0300

    atualizações no main

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from joblib import Parallel, delayed
+def get_atrs(informados):
+    return " ".join(f'{k.split("_")[-1]}="{v}"'
+        for k, v in informados.items())
 
 def criar_blocos(texto, *args, classe="succcess", inline = False):
     tag = "span" if inline else "div"
@@ -8,6 +11,7 @@ def criar_blocos(texto, *args, classe="succcess", inline = False):
 def criar_lista(*itens):
     lista = ''.join((f'\n\t<li>{item}</li>' for item in itens))
     return f'<ul>{lista}\n</ul>'
+
 def sei():
     nome = input()
     print(nome)
